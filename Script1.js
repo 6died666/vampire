2de0f2a800a0146fd36694271c5289abe90c1e3d
6died666 <82220651+6died666@users.noreply.github.com>
2021-04-09 17:06:52 +0300

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,10 @@
+let name = prompt(`your name`);
+const answ = prompt(`enter your age`);
+let color = prompt(`enter your color`);
+alert(`hi_..${color}..${name}/enjoy.reading.I.hope.you.like.it :) `);
+console.log(`hi world`);
+if (number3 > 20) {
+    console.log(`31 > 20`);
+} else {
+    console.log(`3<20`);
+}
